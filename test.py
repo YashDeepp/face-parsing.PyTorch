99c65a6fe76485170c2ad03a0bb292fa36d03387
YashDeepp <89565126+YashDeepp@users.noreply.github.com>
2023-02-14 00:29:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
     # print(vis_parsing_anno_color.shape, vis_im.shape)
-    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
+    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0, vis_parsing_anno_color, 0, 0)
 
     # Save result or not
     if save_im:
